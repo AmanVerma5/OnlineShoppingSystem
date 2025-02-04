31e4b67771def82c61021ec6ef0436b14a22fe61
saurabhp13 <saurabhpawar588@gmail.com>
2025-02-04 20:59:59 +0530

    UserController updated

---
+++ Backend/src/main/java/com/ecom/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.ecom.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecom.dtos.user.RegisterDto;
+import com.ecom.services.IUserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@Autowired
+	private IUserService userService;
+
+	
+	/*
+	 * Desc - user sign up
+	 * URL - http://host:port/users/signup
+	 * Method - POST
+	 * Payload - user req dto
+	 * Success resp - Api resp
+	 * err - Api resp err mesg
+	 */
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@RequestBody RegisterDto registerUserDetails) {
+		return userService.registerUser(registerUserDetails);
+	}
+
+}
