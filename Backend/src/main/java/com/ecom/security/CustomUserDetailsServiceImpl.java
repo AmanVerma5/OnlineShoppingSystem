cd782d993cf90817c7a033d35631d1a4f6d4f09c
saurabhp13 <saurabhpawar588@gmail.com>
2025-02-09 15:06:17 +0530

    Spring Security with JWT added

---
+++ Backend/src/main/java/com/ecom/security/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.ecom.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.ecom.entities.User;
+import com.ecom.exceptions.ResourceNotFoundException;
+import com.ecom.respositories.UserRepository;
+
+public class CustomUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepository.getUserByEmail(email);
+		if (user == null) {
+			throw new ResourceNotFoundException("Email Not Found");
+		}
+		return new CustomUserDetailsImpl(user);
+	}
+
+}
