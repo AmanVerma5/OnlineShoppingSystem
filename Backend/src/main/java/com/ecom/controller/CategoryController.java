d74bed645b8186973d749ecbd65d8747e95a75ea
saurabhp13 <saurabhpawar588@gmail.com>
2025-03-05 22:47:54 +0530

    Category Updation API started

--- Backend/src/main/java/com/ecom/controller/CategoryController.java
+++ Backend/src/main/java/com/ecom/controller/CategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -67,6 +68,15 @@ public class CategoryController {
 
 		} catch (RuntimeException e) {
 			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ApiResponse(e.getMessage()));
+		}
+	}
+
+	@PutMapping("/update/{catId}")
+	public ResponseEntity<?> updateCategory(@PathVariable Integer catId, @RequestBody CategoryReqDto dto) {
+		try {
+			return ResponseEntity.ok(categoryService.updateCategory(catId, dto));
+		} catch (RuntimeException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse(e.getMessage()));
 		}
 	}
 
